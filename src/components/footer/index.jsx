01a82009d16b84cc403f003ef627d6924fb69c3b
Dasher <dasha.topor@gmail.com>
2024-06-17 23:15:31 +0200

    feat: create first version of footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
-    return (
-        <></> 
-    )
+  return (
+    <div className="footer">
+      <div className="socials">
+        <Link to="https://github.com/Dasher8">
+          <FontAwesomeIcon icon="fa-brands fa-github social-icon" />
+        </Link>
+        <Link to="https://www.behance.net/dariatopor">
+          <FontAwesomeIcon icon="fa-brands fa-square-behance social-icon" />
+        </Link>
+        <Link to="https://www.linkedin.com/in/dariatopor/">
+          <FontAwesomeIcon icon="fa-brands fa-linkedin social-icon" />
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./router";
+import "./fontAwesome";
+import "../src/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<Router />);

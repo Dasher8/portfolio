b5943dc3cf18897a7b29615ee3c959b82aa5387c
Dasher <dasha.topor@gmail.com>
2024-06-18 23:57:27 +0200

    refactor: transform title to link

--- src/components/description/index.jsx
+++ src/components/description/index.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../../index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Description({ src, alt, title, context, tags }) {
   return (
     <section className="projects-description-container">
-      <div>
-        <img src={src} alt={alt} />
+      <div className="projects-description-img-container">
+        <img src={src} alt={alt} className="projects-description-img" />
       </div>
-      <div>
-        <h1 className="title">{title}</h1>
+      <div className="about-project">
+        <Link to="https://kasa-iota-flame.vercel.app" className="title-about-project-link">
+        <span className="title-about-project">{title}<FontAwesomeIcon icon="fa-solid fa-link" className="fa-link"/></span>
+        </Link>
         <p className="context">{context}</p>
         <div className="tags-container">
           <ul className="tags-list">
--- src/pages/argent/index.jsx
+++ src/pages/argent/index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import LayoutDefault from "../../layouts/default";
 import projects from "../../../public/data/projects.json";
 import Description from "../../components/description";
+import "../../index.css";
 export default function Argent() {
+
       // Select the project with id "1"
   const projectId = "1";
   const project = projects.data.find(proj => proj.id === projectId);

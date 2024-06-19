d5ad72a45b86f3c7632c74816d8d576b853c2801
Dasher <dasha.topor@gmail.com>
2024-06-19 17:27:27 +0200

    refactor: use local images for projects cards instead of URLs from JSON

--- src/containers/projects/index.jsx
+++ src/containers/projects/index.jsx
@@ -4,16 +4,31 @@ import "../../index.css";
 
 import Project from "../../components/project/index";
 
+import projectImage1 from "../../assets/mockup_devices.png";
+import projectImage2 from "../../assets/mockup_devices.png";
+import projectImage3 from "../../assets/mockup_devices.png";
+
 export default function Projects() {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     fetch("/data/projects.json")
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        setProjects(response.data);
+      .then((response) => response.json())
+      .then((data) => {
+        // Ajouter les images locales aux données du projet
+        const projectsWithImages = data.data.map((project) => {
+          switch (project.id) {
+            case "1":
+              return { ...project, cover: projectImage1 };
+            case "2":
+              return { ...project, cover: projectImage2 };
+            case "3":
+              return { ...project, cover: projectImage3 };
+            default:
+              return project;
+          }
+        });
+        setProjects(projectsWithImages);
       });
   }, []);
 
@@ -21,13 +36,13 @@ export default function Projects() {
     <section className="cards-section">
       <div className="cards-container">
         {projects.map((project, index) => (
-            <Project
-              key={index}
-              id={project.id}
-              title={project.title}
-              cover={project.cover}
-              alt={project.title}
-            />
+          <Project
+            key={index}
+            id={project.id}
+            title={project.title}
+            cover={project.cover}
+            alt={project.title}
+          />
         ))}
       </div>
     </section>
